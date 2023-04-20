bf7ac3f058270e897faf1fcc07399e9d3c69aad4
sebas-777 <sebastiancast06@hotmail.com>
2023-04-20 09:53:28 -0500

    se validan temas blinding , eventos, string interpolacion

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -28,4 +28,9 @@ export class AppComponent {
      const element = event.target as HTMLElement;
      console.log(element.scrollTop);
   }
+
+  changeName(event:Event){
+    const element = event.target as HTMLInputElement;
+   this.person.name = element.value;
+  }
 }
